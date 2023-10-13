f88be27c722bbbfe51fb36365325cb66c78ab65e
Eliasz Nalepka <92109535+Zehlan1@users.noreply.github.com>
2023-10-13 11:27:29 +0200

    big upgrade

--- Lab_2/main.js
+++ Lab_2/main.js
@@ -5,6 +5,12 @@ const slides = document.querySelector('.slides')
 const leftBtn = document.querySelector('#left')
 const rightBtn = document.querySelector('#right')
 
+const firstBtn = document.querySelector('#pic1')
+const secondBtn = document.querySelector('#pic2')
+const thirdBtn = document.querySelector('#pic3')
+const fourthBtn = document.querySelector('#pic4')
+const fifthBtn = document.querySelector('#pic5')
+
 // jednorazowe wykonanie kodu po określonym czasie
 // const timeoutRef = setTimeout(
 //     () => {
@@ -17,11 +23,42 @@ const rightBtn = document.querySelector('#right')
 rightBtn.addEventListener('click', () => {animate(true)})
 leftBtn.addEventListener('click', () => {animate(false)})
 
+firstBtn.addEventListener('click', () => {navigate(1)})
+secondBtn.addEventListener('click', () => {navigate(2)})
+thirdBtn.addEventListener('click', () => {navigate(3)})
+fourthBtn.addEventListener('click', () => {navigate(4)})
+fifthBtn.addEventListener('click', () => {navigate(5)})
+
 // wykonywanie kodu co określony czas
 let licznik = 0
 
 function animate(next) {
     next ? console.log('right clicked') : console.log('left clicked')
+    const intervalRef = setInterval(
+        () => {
+            next ? licznik += 8 : licznik -= 8
+            slides.style.transform = `translate(-${licznik}px)`
+            main.innerHTML = `Msg from setInterval: ${licznik}`
+            if(licznik > 2400)
+            {
+                licznik = 0
+            }
+            if(licznik < 0)
+            {
+                licznik = 2400
+            }
+            if(licznik%600 == 0)
+            {
+                slides.style.transform = `translate(-${licznik}px)`
+                main.innerHTML = `Msg from setInterval: ${licznik}`
+                clearInterval(intervalRef)
+            }
+        },
+        16
+    )
+}
+
+function navigate(dest) {
     const intervalRef = setInterval(
         () => {
             next ? licznik += 8 : licznik -= 8
