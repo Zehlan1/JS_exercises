ff2e097998d2d428ebb8a8efd8088904ee0d6921
Zehlan1 <e.nalepka02@gmail.com>
2023-11-10 00:16:56 +0100

    Lab 3 work continues

--- Lab_3/drumkit.js
+++ Lab_3/drumkit.js
@@ -1,4 +1,5 @@
 document.addEventListener('keypress', onKeyPress)
+document.addEventListener('keypress', writeDown)
 
 const TrackText = {
     1: document.querySelector('#nfo1'),
@@ -28,10 +29,15 @@ const PlayBtn = {
     4: document.querySelector('#play4')
 }
 
-let track1 = {}
-let track2 = {}
-let track3 = {}
-let track4 = {}
+let recording = false
+let currTrack = 0
+
+const Tracks = {
+    1: {},
+    2: {},
+    3: {},
+    4: {}
+}
 
 let timernum = 0
 
@@ -52,9 +58,34 @@ rec2.addEventListener('click', () => {record(2)})
 rec3.addEventListener('click', () => {record(3)})
 rec4.addEventListener('click', () => {record(4)})
 
+document.querySelector('#play1').addEventListener('click', () => {play(1)})
+play2.addEventListener('click', () => {play(2)})
+play3.addEventListener('click', () => {play(3)})
+play4.addEventListener('click', () => {play(4)})
+
+function play(channel) {
+    let curSound = 1
+    const soundsNum = Object.keys(Tracks[channel]).length
+    let ownTimer = 0
+    const timer = setInterval(
+        () => {
+            ownTimer += 1
+            if(ownTimer in Tracks[channel]){
+                playSound(Tracks[channel][ownTimer])
+                curSound += 1
+            }
+            if(curSound>soundsNum){
+                clearInterval(timer)
+            }
+        },
+        1
+    )
+}
+
 function record(channel) {
     StopBtn[channel].style.display = 'flex'
     RecBtn[channel].style.display = 'none'
+    recording = true
     const timer = setInterval(
         () => {
             timernum += 1
@@ -65,12 +96,21 @@ function record(channel) {
                 RecBtn[channel].style.display = 'flex'
                 TrackText[channel].innerHTML = ``
                 timernum = 0
+                recording = false
             })
         },
         1
     )
 }
 
+function writeDown(event) {
+    if(recording){
+        Tracks[currTrack].push({
+            key: timernum,
+            value: KeyToSound[event.key]
+        })
+    }
+}
 function onKeyPress(event) {
     const sound = KeyToSound[event.key]
     playSound(sound)
