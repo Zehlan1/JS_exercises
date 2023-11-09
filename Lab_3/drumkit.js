6da552d4cec052dfd52798631857b0580de57866
Zehlan1 <e.nalepka02@gmail.com>
2023-11-09 23:48:42 +0100

    Lab 3 WIP 2

--- Lab_3/drumkit.js
+++ Lab_3/drumkit.js
@@ -1,16 +1,39 @@
 document.addEventListener('keypress', onKeyPress)
 
-const nfo1 = document.querySelector('#nfo1')
-const nfo2 = document.querySelector('#nfo2')
-const nfo3 = document.querySelector('#nfo3')
-const nfo4 = document.querySelector('#nfo4')
+const TrackText = {
+    1: document.querySelector('#nfo1'),
+    2: document.querySelector('#nfo2'),
+    3: document.querySelector('#nfo3'),
+    4: document.querySelector('#nfo4')
+}
 
-const rec1 = document.querySelector('#rec1')
-const rec2 = document.querySelector('#rec2')
-const rec3 = document.querySelector('#rec3')
-const rec4 = document.querySelector('#rec4')
+const RecBtn = {
+    1: document.querySelector('#rec1'),
+    2: document.querySelector('#rec2'),
+    3: document.querySelector('#rec3'),
+    4: document.querySelector('#rec4')
+}
 
-let timer = 0
+const StopBtn = {
+    1: document.querySelector('#stop1'),
+    2: document.querySelector('#stop2'),
+    3: document.querySelector('#stop3'),
+    4: document.querySelector('#stop4')
+}
+
+const PlayBtn = {
+    1: document.querySelector('#play1'),
+    2: document.querySelector('#play2'),
+    3: document.querySelector('#play3'),
+    4: document.querySelector('#play4')
+}
+
+let track1 = {}
+let track2 = {}
+let track3 = {}
+let track4 = {}
+
+let timernum = 0
 
 const KeyToSound = {
     'a': document.querySelector('#s1'),
@@ -25,18 +48,27 @@ const KeyToSound = {
 }
 
 rec1.addEventListener('click', () => {record(1)})
+rec2.addEventListener('click', () => {record(2)})
+rec3.addEventListener('click', () => {record(3)})
+rec4.addEventListener('click', () => {record(4)})
 
 function record(channel) {
-    let good = true
+    StopBtn[channel].style.display = 'flex'
+    RecBtn[channel].style.display = 'none'
     const timer = setInterval(
         () => {
             timernum += 1
-            nfo1.innerHTML = `${timernum}`
-            good ? null : clearInterval(timer)
+            TrackText[channel].innerHTML = `${timernum}`
+            StopBtn[channel].addEventListener('click', () => {
+                clearInterval(timer)
+                StopBtn[channel].style.display = 'none'
+                RecBtn[channel].style.display = 'flex'
+                TrackText[channel].innerHTML = ``
+                timernum = 0
+            })
         },
-        1000
+        1
     )
-    playSound(sound)
 }
 
 function onKeyPress(event) {
