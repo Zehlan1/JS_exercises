f11d4c4ee27757dfdd44e922df219d1c2b9eaebd
Zehlan1 <e.nalepka02@gmail.com>
2023-12-12 14:44:18 +0100

    Lab_1_Revised

--- Lab_1/main.js
+++ Lab_1/main.js
@@ -1,20 +1,59 @@
-let nums = document.querySelectorAll('input')
-const resultContainer = document.querySelector('#results')
+const inputAddBtn = document.querySelector('#inputAdd')
+const inputs = document.querySelector('#inputsContainer')
+const result = document.querySelector('#result')
+let inputDelBtn = document.querySelectorAll('.inputDelete')
+let inputAmount = 0
 
-nums.forEach(num => {
-    num.addEventListener('input', updateResult)
-})
+inputAddBtn.addEventListener('click', addInput)
+inputs.addEventListener('input', updateResult)
+
+function addInput () {
+    const newInputBox = document.createElement('div')
+    newInputBox.classList.add('inputBox')
+
+    const newInputField = document.createElement('input')
+    newInputField.type = 'text'
+    
+    const newInputDelete = document.createElement('button')
+    newInputDelete.classList.add('inputDelete')
+    newInputDelete.textContent = 'Remove'
+    newInputDelete.addEventListener('click', (element) => {
+        deleteInput(element);
+    })
+
+    newInputBox.appendChild(newInputField)
+    newInputBox.appendChild(newInputDelete)
+    inputs.appendChild(newInputBox)
+    inputAmount++
+}
+
+function deleteInput(element) {
+    const inputBox = element.target.parentElement
+    inputs.removeChild(inputBox)
+    inputAmount--
+    updateResult()
+}
 
 function updateResult() {
-    const num1 = Number(document.querySelector('#num1').value)
-    const num2 = Number(document.querySelector('#num2').value)
-    const num3 = Number(document.querySelector('#num3').value)
-    const num4 = Number(document.querySelector('#num4').value)
-    resultContainer.innerHTML =
-        "Sum = "+(num1+num2+num3+num4)+
-        " Avg = "+((num1+num2+num3+num4)/4)+
-        " Min = "+Math.min(num1, num2, num3, num4)+
-        " Max = "+Math.max(num1, num2, num3, num4)
+    const inputFields = inputs.querySelectorAll('input')
+
+    let sum = 0
+    let min = Number.MAX_VALUE
+    let max = -Number.MAX_VALUE
+
+    inputFields.forEach(input => {
+        const number = Number(input.value)
+        sum += number
+        min = Math.min(min, number)
+        max = Math.max(max, number)
+    })
+
+    const avg = sum / inputAmount
+
+    document.querySelector('#sum').innerHTML = `Sum: ${sum}`
+    document.querySelector('#avg').innerHTML = `Average: ${avg}`
+    document.querySelector('#min').innerHTML = `Min: ${min}`
+    document.querySelector('#max').innerHTML = `Max: ${max}`
 }
 
 
