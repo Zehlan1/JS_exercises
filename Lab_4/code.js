c89743b88684320fd40f831c3197c66fadfb32f3
Zehlan1 <e.nalepka02@gmail.com>
2023-12-14 16:41:45 +0100

    Lab_4 Revised

--- Lab_4/code.js
+++ Lab_4/code.js
@@ -1,24 +1,93 @@
-document.querySelector('#create').addEventListener('click', add_note)
+document.addEventListener("DOMContentLoaded", displayNotes)
+document.querySelector('#addNote').addEventListener('click', addNote)
+document.querySelector('#find').addEventListener('input', findNotes)
 
-let _title = document.querySelector('#title')
-let _content = document.querySelector('#desc')
-let _color = document.querySelector('#color')
-let _pinned = document.querySelector('#marked')
+const noteContainer = document.getElementById("noteContainer")
 
-let cur_id = 1
-let notes = {}
+function addNote() {
+    const title = document.getElementById('title').value
+    const content = document.getElementById('content').value
+    const color = document.getElementById('color').value
+    const tags = document.getElementById('tags').value.trim().split(' ')
+    const fav = document.getElementById('fav').checked
+    const date = new Date().toLocaleString()
 
-function add_note() {
+    const note = {title, content, color, tags, fav, date}
 
-    let note = {
-        title: _title.value,
-        content: _content.value,
-        color: _color.value,
-        pinned: _pinned.value,
-        created: Date.now()
-    }
+    let notes = JSON.parse(localStorage.getItem('notes')) || []
 
-    notes[cur_id] = note
-    cur_id++
+    notes.push(note)
+    localStorage.setItem('notes', JSON.stringify(notes))
+
+    displayNotes()
 }
 
+function deleteNote(index) {
+    let notes = JSON.parse(localStorage.getItem("notes"))
+    notes.splice(index, 1)
+    localStorage.setItem("notes", JSON.stringify(notes))
+
+    displayNotes()
+}
+
+function displayNotes() {
+    noteContainer.textContent = ''
+
+    let notes = JSON.parse(localStorage.getItem('notes')) || []
+
+    const favNotes = notes.filter(note => note.fav)
+    const regNotes = notes.filter(note => !note.fav)
+
+    favNotes.forEach((note, index) => {
+        noteContainer.appendChild(buildNote(note, index))
+    })
+
+    regNotes.forEach((note, index) => {
+        noteContainer.appendChild(buildNote(note, index))
+    })
+}
+
+function buildNote(note, index) {
+    const newNote = document.createElement("div")
+    newNote.classList.add("note")
+
+    let noteTitle = document.createElement('div')
+    noteTitle.classList.add("nTitle")
+    noteTitle.innerHTML = note.title
+    let noteContent = document.createElement('div')
+    noteContent.classList.add("nContent")
+    noteContent.innerHTML = note.content
+    let noteTags = document.createElement('div')
+    noteTags.classList.add("nTags")
+    noteTags.innerHTML = note.tags.length > 0 ? note.tags.join(' ') : ''
+    let noteDate = document.createElement('div')
+    noteDate.classList.add("nDate")
+    noteDate.innerHTML = note.date
+
+    const noteDelete = document.createElement('button')
+    noteDelete.classList.add('noteDelete')
+    noteDelete.setAttribute('onclick',`deleteNote(${index})`);
+    noteDelete.textContent = 'Remove'
+    //noteDelete.addEventListener('click', deleteNote(index))
+
+    newNote.appendChild(noteTitle)
+    newNote.appendChild(noteContent)
+    newNote.appendChild(noteTags)
+    newNote.appendChild(noteDate)
+    newNote.appendChild(noteDelete)
+
+    return newNote
+}
+
+function findNotes() {
+    const prompt = document.getElementById("find").value
+    let notes = [...noteContainer.getElementsByClassName('note')]
+
+    notes.forEach((note) => {
+        const title = note.querySelector('.nTitle').innerHTML
+        const content = note.querySelector('.nTitle').innerHTML
+        const tags = note.querySelector('.nTags').innerHTML
+
+        note.style.display = title.includes(prompt) || content.includes(prompt) || tags.includes(prompt) ? 'block' : 'none'
+    });
+}
